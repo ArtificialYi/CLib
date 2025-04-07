e078f6be6e96fc1f4c2f1f6713b13513f52045fc
huangyi <496109870@qq.com>
2025-04-08 02:34:32 +0800

    test: 初始化文件

--- main.c
+++ main.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
+#include <math.h>
 
-int main() {
-    int a;
-    printf("Hello, World!\n");
-    scanf("%d", &a);
-    printf("You entered: %d\n", a);
+int main()
+{
+    double a, b, c, d, x1, x2;
+
+    // scanf("%lf,%lf,%lf", &a, &b, &c);
+    a = 1;
+    b = 4;
+    c = 3;
+
+    d = sqrt(b * b - 4 * a * c);
+    x1 = (-b + d) / 2;
+    x2 = (-b - d) / 2;
+
+    printf("x1=%lf,x2=%lf\n", x1, x2);
+
     return 0;
 }
